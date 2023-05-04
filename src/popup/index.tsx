8c40a05e7d7784fa7462ac57206feec8d212424c
CoderSerio <CoderSerio@2779066456@qq.com>
2023-05-04 11:45:11 +0800

    feat: 内容注入

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,25 +1,34 @@
 import { useState } from "react"
+import { Tabs, Typography } from '@arco-design/web-react';
+const TabPane = Tabs.TabPane;
+import styles from './index.module.less'
 
 const Popup = () => {
   const [data, setData] = useState("")
+  const [activeTabKey, setActiveTabKey] = useState<string>('1')
+
+  const handleTabChange = (tabKey: string) => {
+    debugger
+    setActiveTabKey(tabKey)
+  }
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className={styles['wrapper']}>
+      <Tabs activeTab={activeTabKey} onChange={handleTabChange} className={styles['tabs']}>
+        
+        <TabPane key='1' title='登录'>
+          <div>123</div>
+        </TabPane>
+        
+        <TabPane key='2' title='游戏'>
+          <div>23</div>
+        </TabPane>
+
+        <TabPane key='3' title='设置'>
+          <div>345</div>
+        </TabPane>
+      
+      </Tabs>
     </div>
   )
 }
